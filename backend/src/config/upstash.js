52a2320d5a8438450f230eb07c76cf4003246163
Gaurav Sahani <gauravsahani010@gmail.com>
2025-07-17 12:29:14 +0530

    updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -12,7 +12,7 @@ const redis = new Redis({
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.fixedWindow(10, "10 s"), 
+  limiter: Ratelimit.fixedWindow(100, "60 s"), 
   analytics: true,
 });
 
